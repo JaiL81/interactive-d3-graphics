f3972160b7b016092337ee450f16d705ccdd3dfb
Jaime <jail81@gmail.com>
2016-01-18 22:51:02 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interactiveD3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the interactiveD3App
+ */
+angular.module('interactiveD3App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
