9494918de4921761ad0fda1be302a6fe1dcdc1c2
Jaime <jail81@gmail.com>
2016-01-24 20:20:15 +0100

    Several features added.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,7 +23,7 @@ angular
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
-        controllerAs: 'main'
+        controllerAs: 'vm'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,54 +9,180 @@
  */
 angular.module('interactiveD3App')
   .controller('MainCtrl', function ($http) {
-    var main = this;
+    var vm = this;
 
     $http.get("../../json/data.json").then(function (response) {
+      vm.data = response.data;
+      vm.currentYear = vm.data[0].year;
+
       buildPopulationGraph(response.data[0].population);
-    });
 
 
-    function buildPopulationGraph(data) {
-
-      // SET UP DIMENSIONS
-  //    var w = parseInt(d3.select(".population").attr("width")),
-    //    h = parseInt(d3.select(".population").attr("height"));
-    var w = 300,
-        h = 300;
-
-      var margin = {
-        top: 20,
-        right: 20,
-        bottom: 24,
-        left: 20
+      vm.options = {
+        chart: {
+          type: 'multiChart',
+          height: 400,
+          width: 600,
+          margin : {
+            top: 30,
+            right: 60,
+            bottom: 50,
+            left: 70
+          },
+          color: d3.scale.category10().range(),
+          useInteractiveGuideline: true,
+          duration: 500,
+          xAxis: {
+            tickFormat: function(d){
+              return d3.format(',f')(d);
+            }
+          },
+          yAxis1: {
+            tickFormat: function(d){
+              return d3.format(',.1f')(d);
+            }
+          },
+          yAxis2: {
+            tickFormat: function(d){
+              return d3.format(',.1f')(d);
+            }
+          }
+        }
       };
 
-  // the width of each side of the chart
-      var regionWidth = w / 2;
+      vm.multiLineData = [
+        {
+          key: 'Workers per dependent',
+          type: 'line',
+          yAxis: 1,
+          values: _.map(vm.data, function (d) {
+            return {x: d.year, y: d.workersPerDependent};
+          })
+        },
+        {
+          key: 'Income per person',
+          type: 'line',
+          yAxis: 2,
+          values: _.map(vm.data, function (d) {
+            return {x: d.year, y: d.incomePerPerson};
+          })
+        }
+      ];
 
-  // these are the x-coordinates of the y-axes
-      var pointA = regionWidth,
-        pointB = w - regionWidth;
 
-  // GET THE TOTAL POPULATION SIZE AND CREATE A FUNCTION FOR RETURNING THE PERCENTAGE
-      var totalPopulation = d3.sum(data, function (d) {
-          return d.male + d.female;
-        }),
-        percentage = function (d) {
-          return d / totalPopulation;
-        };
+        console.log(_.map(vm.data, function (d) {
+          return {x: d.year, y: d.workersPerDependent};
+        }))
+
+    });
+    // SET UP DIMENSIONS
+    //    var w = parseInt(d3.select(".population").attr("width")),
+    //    h = parseInt(d3.select(".population").attr("height"));
+    var w = 300,
+      h = 300;
+
+    var margin = {
+      top: 20,
+      right: 20,
+      bottom: 24,
+      left: 20
+    };
+
+    // the width of each side of the chart
+    var regionWidth = w / 2;
+
+    // these are the x-coordinates of the y-axes
+    var pointA = regionWidth,
+      pointB = w - regionWidth;
+
+    // GET THE TOTAL POPULATION SIZE AND CREATE A FUNCTION FOR RETURNING THE PERCENTAGE
+    var totalPopulation, percentage;
 
 
-  // CREATE SVG
-      var svg = d3.select('svg')
-        .attr('width', margin.left + w + margin.right)
-        .attr('height', margin.top + h + margin.bottom)
-        // ADD A GROUP FOR THE SPACE WITHIN THE MARGINS
-        .append('g')
-        .attr('transform', translation(margin.left, margin.top));
+    function calculatePercentage(data) {
+      totalPopulation = d3.sum(data, function (d) {
+        return d.male + d.female;
+      });
 
-  // find the maximum data value on either side
-  //  since this will be shared by both of the x-axes
+      percentage = function (d) {
+        return d / totalPopulation;
+      };
+    }
+
+
+    vm.updatePopulationChart = function () {
+      vm.currentYear++;
+      var population = _.find(vm.data, {year: vm.currentYear}).population;
+      var leftBarGroup = d3.select('svg').select('#leftGroup').selectAll('.bar.left')
+        .data(population);
+      var rightBarGroup = d3.select('svg').select('#rightGroup');
+
+
+      var scales = setUpScales(population);
+      var yScale = scales.yScale;
+      var xScale = scales.xScale;
+
+      //drawGroupBars(leftBarGroup, population, scales.yScale, scales.xScale, rightBarGroup);
+      leftBarGroup
+        .enter().append('rect')
+        .attr('class', 'bar left')
+        .attr('x', 0)
+        .attr('y', function (d) {
+          return yScale(d.group);
+        })
+        .attr('width', function (d) {
+          return xScale(percentage(d.male));
+        })
+        .attr('height', yScale.rangeBand());
+
+      /*rightBarGroup.selectAll('.bar.right')
+        .data(population)
+        .enter().append('rect')
+        .attr('class', 'bar right')
+        .attr('x', 0)
+        .attr('y', function (d) {
+          return yScale(d.group);
+        })
+        .attr('width', function (d) {
+          return xScale(percentage(d.female));
+        })
+        .attr('height', yScale.rangeBand());
+*/
+     // leftBarGroup.exit().remove();
+      //rightBarGroup.exit().remove();
+    };
+
+    function drawGroupBars(leftBarGroup, data, yScale, xScale, rightBarGroup) {
+      leftBarGroup.selectAll('.bar.left')
+        .data(data)
+        .enter().append('rect')
+        .attr('class', 'bar left')
+        .attr('x', 0)
+        .attr('y', function (d) {
+          return yScale(d.group);
+        })
+        .attr('width', function (d) {
+          return xScale(percentage(d.male));
+        })
+        .attr('height', 15);
+
+      rightBarGroup.selectAll('.bar.right')
+        .data(data)
+        .enter().append('rect')
+        .attr('class', 'bar right')
+        .attr('x', 0)
+        .attr('y', function (d) {
+          return yScale(d.group);
+        })
+        .attr('width', function (d) {
+          return xScale(percentage(d.female));
+        })
+        .attr('height', 15);
+    }
+
+    function setUpScales(data) {
+      // find the maximum data value on either side
+      //  since this will be shared by both of the x-axes
       var maxValue = Math.max(
         d3.max(data, function (d) {
           return percentage(d.male);
@@ -66,10 +192,10 @@ angular.module('interactiveD3App')
         })
       );
 
-  // SET UP SCALES
+      // SET UP SCALES
 
-  // the xScale goes from 0 to the width of a region
-  //  it will be reversed for the left x-axis
+      // the xScale goes from 0 to the width of a region
+      //  it will be reversed for the left x-axis
       var xScale = d3.scale.linear()
         .domain([0, maxValue])
         .range([0, regionWidth])
@@ -80,9 +206,27 @@ angular.module('interactiveD3App')
           return d.group;
         }))
         .rangeRoundBands([h, 0], 0.1);
+      return {xScale: xScale, yScale: yScale};
+    }
+
+    function buildPopulationGraph(data) {
 
 
-  // SET UP AXES
+      calculatePercentage(data);
+
+      var svg = d3.select('svg')
+        .attr('width', margin.left + w + margin.right)
+        .attr('height', margin.top + h + margin.bottom)
+        // ADD A GROUP FOR THE SPACE WITHIN THE MARGINS
+        .append('g')
+        .attr('transform', translation(margin.left, margin.top));
+
+      var scales = setUpScales(data);
+      var xScale = scales.xScale;
+      var yScale = scales.yScale;
+
+
+      // SET UP AXES
       var xAxisRight = d3.svg.axis()
         .scale(xScale)
         .orient('bottom')
@@ -96,7 +240,7 @@ angular.module('interactiveD3App')
         .tickValues(xScale.domain())
         .tickFormat(d3.format('%'));
 
-  // DRAW AXES
+      // DRAW AXES
       svg.append('g')
         .attr('class', 'axis x left')
         .attr('transform', translation(0, h))
@@ -107,42 +251,18 @@ angular.module('interactiveD3App')
         .attr('transform', translation(pointB, h))
         .call(xAxisRight);
 
-  // MAKE GROUPS FOR EACH SIDE OF CHART
-  // scale(-1,1) is used to reverse the left side so the bars grow left instead of right
+      // MAKE GROUPS FOR EACH SIDE OF CHART
+      // scale(-1,1) is used to reverse the left side so the bars grow left instead of right
       var leftBarGroup = svg.append('g')
+        .attr('id', 'leftGroup')
         .attr('transform', translation(pointA, 0) + 'scale(-1,1)');
       var rightBarGroup = svg.append('g')
+        .attr('id', 'rightGroup')
         .attr('transform', translation(pointB, 0));
 
-  // DRAW BARS
-      leftBarGroup.selectAll('.bar.left')
-        .data(data)
-        .enter().append('rect')
-        .attr('class', 'bar left')
-        .attr('x', 0)
-        .attr('y', function (d) {
-          return yScale(d.group);
-        })
-        .attr('width', function (d) {
-          return xScale(percentage(d.male));
-        })
-        .attr('height', yScale.rangeBand());
-
-      rightBarGroup.selectAll('.bar.right')
-        .data(data)
-        .enter().append('rect')
-        .attr('class', 'bar right')
-        .attr('x', 0)
-        .attr('y', function (d) {
-          return yScale(d.group);
-        })
-        .attr('width', function (d) {
-          return xScale(percentage(d.female));
-        })
-        .attr('height', yScale.rangeBand());
+      drawGroupBars(leftBarGroup, data, yScale, xScale, rightBarGroup);
 
 
-  // so sick of string concatenation for translations
       function translation(x, y) {
         return 'translate(' + x + ',' + y + ')';
       }
